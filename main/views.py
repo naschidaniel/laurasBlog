70c60759384967868bd08eed638775d73d01ca47
naschidaniel <daniel@naschi.at>
2019-11-10 18:33:28 +0100

    django: add main site

--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello World Index Page")
+    """
+       Public interface to the flat page view.
+       Templates:
+       Context:
+           flatpage
+               `flatpages.flatpages` object
+    """
+    context = {}
+
+    return render(request, 'main/main.html', context)
 
