05515cb463b77f4371bc3f09115f1d57253e2139
daniel <daniel@localhost.localdomain>
2019-12-02 18:55:17 +0100

    VueJS: footer.vue to AppFooter.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="app">
     <div class="antialiased flex flex-col min-h-screen">
-      <top></top>
+      <app-top />
       <navbar></navbar>
       <main class=" mx-auto flex-grow lg:max-w-6xl">
         <router-view />
         <BlogCategories></BlogCategories>
       </main>
-      <Footer></Footer>
+      <app-Footer />
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Top from "./components/Top.vue";
+import AppTop from "./components/AppTop.vue";
 import BlogCategories from "./components/BlogCategories.vue";
-import Footer from "./components/Footer.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default {
   name: "app",
   components: {
     Navbar,
     BlogCategories,
-    Top,
-    Footer
+    AppTop,
+    AppFooter
   }
 };
 </script>
