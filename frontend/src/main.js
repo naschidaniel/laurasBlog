25845112fd1eae89f09f76cc348c891e9989c7cf
daniel <daniel@localhost.localdomain>
2019-12-11 19:52:33 +0100

    add store to Vue app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import App from "./components/App.vue";
 import router from "./router";
+import store from "./store";
 import "@/assets/css/tailwind.css";
+
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount("#app");
