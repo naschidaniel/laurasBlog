c26649c61fccb28370565613af783a08f4e89b1f
Daniel Naschberger <daniel@localhost.localdomain>
2020-01-10 13:13:16 +0100

    refact allBlogCategories

--- frontend/src/components/BlogCategories.vue
+++ frontend/src/components/BlogCategories.vue
@@ -1,19 +1,8 @@
 <template>
   <div class="pt-4 px-4 pb-2">
     <h3 class="uppercase">Kategorien</h3>
-    {{ getblogCategoryById(1) }}
-    <ul>
-      <li v-for="bC in allBlogCateogries" :key="bC.category" class="pt-3">
-        <span v-if="bC.breadcrumps.length == 1">{{ bC.breadcrumps[0] }}</span>
-        <ul v-else>
-          <li>
-            <ul>
-              <li class=" px-6">{{ bC.breadcrumps[1] }}</li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <!-- {{ getblogCategoryById(1) }} -->
+    {{ allBlogCateogries }}
   </div>
 </template>
 
