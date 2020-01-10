3e6d47f3bd6c21f8123c6b8f9fb0d95755f352be
Daniel Naschberger <daniel@localhost.localdomain>
2020-01-10 12:05:13 +0100

    moved main to trash

--- laurasBlog/settings.py
+++ laurasBlog/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
-    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
