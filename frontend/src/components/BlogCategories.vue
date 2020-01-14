40c6b7388d84eba470d134fde4ba650914438d46
daniel <daniel@localhost.localdomain>
2020-01-14 17:05:04 +0100

    replace vueJS mounted with created

--- frontend/src/components/BlogCategories.vue
+++ frontend/src/components/BlogCategories.vue
@@ -16,7 +16,7 @@ import { mapGetters } from "vuex";
 export default {
   name: "blogCategory",
   computed: mapGetters(["allBlogCateogries", "getLoadingStatus"]),
-  mounted() {
+  created() {
     this.$store.dispatch("fetchBlogCategories");
   }
 };
