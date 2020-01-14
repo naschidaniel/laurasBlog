67dde649be9eca173e875245aeba82755ef61b46
daniel <daniel@localhost.localdomain>
2020-01-14 19:55:37 +0100

    introduce a a stus check

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -3,8 +3,12 @@ import axios from "axios";
 export async function api(apiLink) {
   try {
     const response = await axios.get(apiLink);
-    const data = await response.data;
-    return data
+    if (response.status == 200) {
+      const data = await response.data;
+      return data
+    } else {
+      console.log("Api Status: " + response.status)
+    }
   } catch (error) {
       console.log(error);
   }
