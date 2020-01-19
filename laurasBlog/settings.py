0963810d1b531d8ee8b703c6964aa23038b7c162
daniel <daniel@localhost.localdomain>
2020-01-19 10:18:53 +0100

    setting up the staticfiles_dir

--- laurasBlog/settings.py
+++ laurasBlog/settings.py
@@ -137,10 +137,13 @@ REST_FRAMEWORK = {
     ]
 }
 
+
+STATICFILES_DIRS = (os.path.join(FRONTEND_DIR, 'dist'),)
+
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': DEBUG,
-        'BUNDLE_DIR_NAME': '/bundles/',  # must end with slash
+        'BUNDLE_DIR_NAME': 'dist/',  # must end with slash
         'STATS_FILE': os.path.join(FRONTEND_DIR, 'webpack-stats.json'),
     }
 }
