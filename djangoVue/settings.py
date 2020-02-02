648f6dfdbc189f03203c5a5c57f644744c62afa2
daniel <daniel@localhost.localdomain>
2020-02-02 15:28:22 +0100

    set example data

--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -95,13 +95,6 @@ WSGI_APPLICATION = 'djangoVue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 if env('DB') == 'postgres':
     DATABASES = {
         'default': {
