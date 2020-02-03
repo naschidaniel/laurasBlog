9a32fd768c3ff3f39670febae3b06b34c9432803
daniel <daniel@localhost.localdomain>
2020-02-03 22:30:27 +0100

    make eslint happy

--- frontend/src/components/Blog.vue
+++ frontend/src/components/Blog.vue
@@ -6,21 +6,23 @@
         :key="bp.title"
         class="w-full px-2 mt-3 md:w-1/2 "
       >
-        <div
-          class="shadow-md overflow-hidden"
-        >
-          <div class="bg-cover bg-center" style="background-image: url('https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80')">
-            <router-link
-              :to="{ name: 'blogpost', params: { blogID: bp.id } }"
-            > 
-              <div class="flex content-center flex-wrap font-blogTitle bg-center h-64">
+        <div class="shadow-md overflow-hidden">
+          <div
+            class="bg-cover bg-center"
+            style="background-image: url('https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80')"
+          >
+            <router-link :to="{ name: 'blogpost', params: { blogID: bp.id } }">
+              <div
+                class="flex content-center flex-wrap font-blogTitle bg-center h-64"
+              >
                 <div class=" bg-gray-200 text-center w-1/2">
                   <span
                     v-for="c in getblogCategoriesById(bp.category).breadcrumps"
                     :key="c"
                     class="text-l text-red-600 uppercase"
                     >{{ c }}
-                  </span> <br>
+                  </span>
+                  <br />
                   <h3 class="text-2XL text-gray-900">
                     {{ bp.title }}
                   </h3>
