fe253c3d69e5478856b66864007450eab50448e8
daniel <daniel@localhost.localdomain>
2020-02-07 20:04:45 +0100

    removed blogCategories

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -6,7 +6,7 @@
       <main class=" mx-auto flex-grow w-full px-3 lg:max-w-6xl">
         <router-view />
       </main>
-      <BlogCategories class="mx-auto w-full px-3 lg:max-w-6xl"></BlogCategories>
+      <!-- <BlogCategories class="mx-auto w-full px-3 lg:max-w-6xl"></BlogCategories> -->
       <app-Footer />
     </div>
   </div>
@@ -15,7 +15,6 @@
 <script>
 import Navbar from "./Navbar.vue";
 import AppTop from "./AppTop.vue";
-import BlogCategories from "./BlogCategories.vue";
 import AppFooter from "./AppFooter.vue";
 import "es6-promise/auto";
 
@@ -23,7 +22,6 @@ export default {
   name: "app",
   components: {
     Navbar,
-    BlogCategories,
     AppTop,
     AppFooter
   }
