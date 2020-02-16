76979ce6815ba94da30bcb3469f7ad51f28515a4
daniel <daniel@localhost.localdomain>
2020-02-16 13:46:36 +0100

    added BlogTop

--- frontend/src/views/Blog.vue
+++ frontend/src/views/Blog.vue
@@ -1,15 +1,20 @@
 <template>
-  <Blog />
+  <div>
+    <Blog-top />
+    <Blog />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Blog from "@/components/Blog.vue";
+import BlogTop from "@/components/BlogTop.vue";
 
 export default {
   name: "blog",
   components: {
-    Blog
+    Blog,
+    BlogTop
   }
 };
 </script>
