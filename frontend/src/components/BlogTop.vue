2dd52ec7fcc9a8540a1199394c4aeedf728ae6d8
daniel <daniel@localhost.localdomain>
2020-02-16 13:46:06 +0100

    split up AppTop and BlogTop

---
+++ frontend/src/components/BlogTop.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="min-w-full mx-auto lg:max-w-6xl">
+    <div
+      class="font-title truncate text-center h-18 md:h-44 mt-2 align-middle px-3"
+    >
+      <div
+        class="flex flex-wrap justify-center ml-3 mr-3 mt-2 mb-2 font-semibold text-xs md:text-l"
+      >
+        <span v-for="(bC, i) in allBlogCateogries" :key="bC.id" class="m-1">
+          <span
+            v-if="i == 0"
+            class="cursor-pointer p-1 md:p-2 text-gray-700 rounded m-2"
+            v-on:click="$store.commit('SET_BLOG_CATEGORY', '')"
+            v-bind:class="selectCategory('')"
+            >All ({{ filterBlogPostsByCategory("").length }})</span
+          >
+          <span v-if="i == 0" class="hidden md:inline">-</span>
+          <span
+            class="cursor-pointer p-1 md:p-2 text-gray-700 rounded m-2"
+            v-on:click="$store.commit('SET_BLOG_CATEGORY', bC.id)"
+            v-bind:class="selectCategory(bC.id)"
+            >{{ bC.category }} ({{
+              filterBlogPostsByCategory(bC.id).length
+            }})</span
+          >
+          <span
+            v-if="i != allBlogCateogries.length - 1"
+            class="hidden md:inline"
+            >-</span
+          >
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "blogCategory",
+  computed: mapGetters([
+    "allBlogCateogries",
+    "getBlogCategory",
+    "filterBlogPostsByCategory"
+  ]),
+  created() {
+    this.$store.dispatch("fetchBlogCategories");
+  },
+  methods: {
+    selectCategory(category) {
+      return {
+        "bg-gray-400": category === this.getBlogCategory,
+        "bg-gray-100": category !== this.getBlogCategory,
+        "hover:bg-gray-400": category !== this.getBlogCategory
+      };
+    }
+  }
+};
+</script>
