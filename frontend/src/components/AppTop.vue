6c3fb2ba0fcb74d5f76a762f089df8ae02d1f63d
daniel <daniel@localhost.localdomain>
2020-02-16 13:58:24 +0100

    fix style

--- frontend/src/components/AppTop.vue
+++ frontend/src/components/AppTop.vue
@@ -3,7 +3,8 @@
     <div
       class="font-title truncate text-center h-18 md:h-44 mt-2 md:mt-4 align-middle px-3"
     >
-      <router-link to="/"
+      <router-link
+        to="/"
         class="text-3xl md:text-5xl ml-3 mr-3 truncate cursor-pointer"
       >
         <span class="text-gray-900">Milena</span>
