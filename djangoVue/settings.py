52c9a745627809aa4850fdc3711a9d9f029f4f13
daniel <daniel@localhost.localdomain>
2020-02-29 21:26:57 +0100

    installation of sociallinks

--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
+    'sociallinks.apps.SociallinksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
