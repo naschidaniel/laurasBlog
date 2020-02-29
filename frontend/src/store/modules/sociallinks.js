3a7052cc06d76f3ee5122fc5696ba50f7c9538ac
daniel <daniel@localhost.localdomain>
2020-02-29 21:53:12 +0100

    added sociallinks to store

---
+++ frontend/src/store/modules/sociallinks.js
@@ -0,0 +1,37 @@
+import { api } from "@/api/api";
+
+export default {
+  state: {
+    loadStatusSocialLinks: [],
+    socialLinks: []
+  },
+  mutations: {
+    SET_LOAD_STATUS_SOCIAL_LINKS(state, loadStatusSocialLinks) {
+      state.loadStatusSocialLinks = loadStatusSocialLinks;
+    },
+    SET_SOCIAL_LINKS(state, socialLinks) {
+      state.socialLinks = socialLinks;
+    }
+  },
+  actions: {
+    fetchSocialLinks({ commit }) {
+      commit("SET_LOAD_STATUS_SOCIAL_LINKS", "loading");
+      async function setSocialLinks() {
+        let apiLink = "/api/socialmedialinks/?format=json";
+        let res = await api(apiLink);
+        commit("SET_SOCIAL_LINKS", res);
+      }
+      setSocialLinks();
+      commit("SET_LOAD_STATUS_SOCIAL_LINKS", "notloading");
+    }
+  },
+  getters: {
+    getSocialLinkByPlatform: state => Platform => {
+      if (state.socialLinks === "") {
+        return "";
+      } else {
+        return state.socialLinks.find(socialLinks => socialLinks.socialMediaPlatform === String(Platform));
+      }
+    }
+  }
+};
