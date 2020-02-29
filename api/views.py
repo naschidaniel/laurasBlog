177fefae9e277adb58a41241909f1a3a21cd0c98
daniel <daniel@localhost.localdomain>
2020-02-29 21:27:20 +0100

    add sociallinks to api

--- api/views.py
+++ api/views.py
@@ -2,9 +2,17 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from blog.models import BlogPost, BlogCategory
 from pages.models import Pages
+from sociallinks.models import SocialMediaLink
 
-from api.serializers import BlogPostSerializer, PagesSerializer, BlogCategorySerializer
+from api.serializers import BlogCategorySerializer, BlogPostSerializer, PagesSerializer, SocialMediaLinkSerializer
 
+
+class BlogCategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = BlogCategory.objects.all()
+    serializer_class = BlogCategorySerializer
 
 class BlogPostViewSet(viewsets.ModelViewSet):
     """
@@ -14,16 +22,16 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     param = 'fadsfdafad'
     serializer_class = BlogPostSerializer
 
-class BlogCategoryViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = BlogCategory.objects.all()
-    serializer_class = BlogCategorySerializer
-
 class PagesViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Pages.objects.all()
     serializer_class = PagesSerializer
+
+class SocialMediaLinkViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = SocialMediaLink.objects.all()
+    serializer_class = SocialMediaLinkSerializer
