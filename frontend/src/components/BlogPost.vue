8d0209e0727a83c37050961017efef52af856069
daniel <daniel@localhost.localdomain>
2020-03-02 20:43:03 +0100

    fix bug with markdown in diffrent file

--- frontend/src/components/BlogPost.vue
+++ frontend/src/components/BlogPost.vue
@@ -10,7 +10,7 @@
     />
     <div
       v-html="getBlogById(blogID).content"
-      class="content mt-5 text-base content-center"
+      class="content mt-5 text-base content-center text-gray-700"
     ></div>
     <div class=" mx-auto mt-5 font-semibold text-lg md:text-2xl">
       <router-link
@@ -82,4 +82,40 @@ export default {
 };
 </script>
 
-<style scoped src="./markdown.css"></style>
+<style scoped>
+img {md:w-1/3} 
+img {md:mr-2} 
+img {md:ml-2} 
+
+.content >>> h1 {
+@apply text-4xl text-red-600 mt-16;
+}
+
+.content >>> h2 {
+@apply text-3xl text-red-600 mt-12;
+}
+
+.content >>> h3 {
+@apply text-2xl text-red-600 mt-8;
+}
+
+.content >>> h4 {
+@apply text-xl text-red-600 mt-4;
+}
+
+.content >>> ul {
+@apply list-inside;
+}
+
+.content >>> li {
+@apply list-disc;
+}
+
+.content >>> p {
+@apply flex flex-wrap max-w-full justify-center p-2 mb-3 text-2xl text-justify;
+}
+
+.content >>> img {
+@apply rounded shadow-2xl;
+}
+</style>
--- frontend/src/components/Pages.vue
+++ frontend/src/components/Pages.vue
@@ -3,7 +3,7 @@
     <h1>
       {{ getPage.title }}
     </h1>
-    <div v-html="getPage.content" class="content"></div>
+    <div v-html="getPage.content" class="content text-gray-700"></div>
   </div>
 </template>
 
@@ -26,4 +26,42 @@ export default {
 };
 </script>
 
-<style scoped src="./markdown.css"></style>
+<style scoped>
+img {md:w-1/3} 
+img {md:mr-2} 
+img {md:ml-2} 
+
+.content >>> h1 {
+@apply text-4xl text-red-600 mt-16;
+}
+
+.content >>> h2 {
+@apply text-3xl text-red-600 mt-12;
+}
+
+.content >>> h3 {
+@apply text-2xl text-red-600 mt-8;
+}
+
+.content >>> h4 {
+@apply text-xl text-red-600 mt-4;
+}
+
+.content >>> ul {
+@apply list-inside;
+}
+
+.content >>> li {
+@apply list-disc;
+}
+
+.content >>> p {
+@apply flex flex-wrap max-w-full justify-center p-2 mb-3 text-2xl text-justify;
+}
+
+.content >>> img {
+@apply rounded shadow-2xl;
+}
+</style>
+
+
