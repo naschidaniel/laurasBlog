af8605f4fbde55265b5f63e88054a69fbf89a8c2
daniel <daniel@localhost.localdomain>
2020-03-14 17:58:25 +0100

    update docker file

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,9 +3,7 @@ import multiprocessing
 command = "gunicorn-3"
 pythonpath = "/www/site"
 bind = "0.0.0.0:3000"
-#workers = multiprocessing.cpu_count() * 2 + 1
 workers = multiprocessing.cpu_count() * 2 + 1
-#workers = 4
 user = "root"
 log_file = ""
 capture_output = True
