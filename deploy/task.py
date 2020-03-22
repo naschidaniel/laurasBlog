9f2f716d8331bd38aa187ba43d203a92d1e4ba81
daniel <daniel@localhost.localdomain>
2020-03-22 11:37:30 +0100

    clear static files folder and migrate

--- deploy/task.py
+++ deploy/task.py
@@ -3,7 +3,9 @@
 """The fabricfile of the project."""
 
 import os
+import shutil
 from invoke import task, Collection, Program
+
 
 
 def docker_compose(c, cmd, **kwargs):
@@ -18,6 +20,29 @@ def docker_compose(c, cmd, **kwargs):
 
     return c.run(" ".join(command), **kwargs)
 
+def manage_py(c, cmd, **kwargs):
+    """The function is used to start a command inside a django container."""
+    uid = "{}:{}".format(os.getuid(), os.getgid())
+    return docker_compose(c, f"run -u {uid} django python3 /www/site/manage.py {cmd}", pty=True)
+
+
+
+### Tasks
+
+# Testing
+@task
+def start(c):
+    """This function is used to start the production test environment."""
+    static_folder = os.path.join(os.getcwd(), "django/static")
+    shutil.rmtree(static_folder)
+    print("{} Static Files Folder cleared".format(static_folder))
+    
+    manage_py(c, "migrate", pty=True)
+    manage_py(c, "collectstatic --no-input", pty=True)
+    docker_compose(c, f"up")
+
+
+# Development
 @task
 def rebuild(c):
     """This function is used to recreate the docker containers."""
@@ -31,15 +56,18 @@ def npm(c, cmd, **kwargs):
     docker_compose(c, f"run -u {uid} vue npm {cmd}", pty=True)
 
 @task
+def build(c, **kwargs):
+    """This function is used to respond to the packet manager npm."""
+    uid = "{}:{}".format(os.getuid(), os.getgid())
+    docker_compose(c, f"run -u {uid} vue npm run build", pty=True)
+    manage_py(c, "migrate")
+    manage_py(c, "collectstatic -v 0 --no-input")
+
+
+@task
 def serve(c):
     """This function is used to start the development environment."""
     docker_compose(c, f"up")
-
-@task
-def managepy(c, cmd, **kwargs):
-    """The function is used to start a command inside a django container."""
-    uid = "{}:{}".format(os.getuid(), os.getgid())
-    return docker_compose(c, f"run -u {uid} django python3 /www/site/manage.py {cmd}", pty=True)
 
 @task
 def djangoup(c, **kwargs):
@@ -49,21 +77,39 @@ def djangoup(c, **kwargs):
 
 MAIN_COLLECTION = Collection()
 
-LOCAL_NS = Collection("local")
-LOCAL_NS.configure({
+####### Testing Collection
+TEST_NS = Collection("test")
+MAIN_COLLECTION.add_collection(TEST_NS)
+TEST_NS.configure({
     "host": "local",
     "hostname": "local",
     "docker_compose_files": [
-        "./docker-compose.yml"
+        "./docker-compose.test.yml"
     ]
 })
-MAIN_COLLECTION.add_collection(LOCAL_NS)
+TEST_NS.add_task(start)
 
-LOCAL_NS.add_task(djangoup)
-LOCAL_NS.add_task(managepy)
-LOCAL_NS.add_task(serve)
-LOCAL_NS.add_task(npm)
-LOCAL_NS.add_task(rebuild)
 
+
+####### Development Collection
+DEV_NS = Collection("dev")
+MAIN_COLLECTION.add_collection(DEV_NS)
+
+DEV_NS.configure({
+    "host": "local",
+    "hostname": "local",
+    "docker_compose_files": [
+        "./docker-compose.dev.yml"
+    ]
+})
+
+DEV_NS.add_task(djangoup)
+DEV_NS.add_task(serve)
+DEV_NS.add_task(build)
+DEV_NS.add_task(npm)
+DEV_NS.add_task(rebuild)
+
+
+####### Program
 PROGRAM = Program(namespace=MAIN_COLLECTION)
 PROGRAM.run()
